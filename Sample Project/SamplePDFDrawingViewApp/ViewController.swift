4906af5e570253d2c2c52a03936f42a3276f00cd
jrosen081 <jrosen081@gmail.com>
2019-02-22 14:56:32 -0500

    Added a sample project

---
+++ Sample Project/SamplePDFDrawingViewApp/ViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ViewController.swift
+//  SamplePDFDrawingViewApp
+//
+//  Created by Jack Rosen (New User) on 2/21/19.
+//  Copyright © 2019 Jack Rosen. All rights reserved.
+//
+
+import UIKit
+import DrawingPDF
+import PDFKit
+
+class ViewController: UIViewController, PDFDelegate {
+	/**
+	 * This view is on the screen so that the PDFView is not blocked off
+	 */
+	@IBOutlet weak var holdingView: UIView!
+	/**
+     * A variable to hold onto the current page
+     */
+	var currentPage: Int = 0
+	/**
+	 * The PDF Drawing View
+	 */
+	var pdfView: PDFDrawingView!
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Gets the document, puts it into the PDFDrawingView and adds it to the view
+		if let path = Bundle.main.path(forResource: "ListAbstractions", ofType: "pdf"), let pdf = PDFDocument(url: URL(fileURLWithPath: path)){
+			pdfView = PDFDrawingView(frame: self.holdingView.frame, document: pdf, delegate: self)
+			self.holdingView.addSubview(pdfView)
+		}
+	}
+	// MARK: These will all be called after a button is pressed
+	
+	
+	// Changes the key to erasing
+	@IBAction func changeToErase(_ sender: Any) {
+		pdfView.drawingKey = PDFDrawingView.DrawingKeys.erase
+	}
+	
+	// Changes the key to lasso
+	@IBAction func changeToLasso(_ sender: Any){
+		pdfView.drawingKey = PDFDrawingView.DrawingKeys.lasso
+	}
+	
+	// Changes the key to drawing
+	@IBAction func changeToScroll(_ sender: Any) {
+		pdfView.drawingKey = PDFDrawingView.DrawingKeys.scroll
+	}
+	
+	// Changes the key to text
+	@IBAction func changeToText(_ sender: Any) {
+		pdfView.drawingKey = PDFDrawingView.DrawingKeys.text
+	}
+	
+	// Changes the key to highlight
+	@IBAction func changeToHighlight(_ sender: Any) {
+		pdfView.drawingKey = PDFDrawingView.DrawingKeys.highlight
+	}
+	
+	// Changes the key to draw
+	@IBAction func changeToDraw(_ sender: Any) {
+		pdfView.drawingKey = PDFDrawingView.DrawingKeys.draw
+	}
+	
+	// MARK: Delegate methods being implemented
+	func scrolled(to page: Int) {
+		self.currentPage = page
+	}
+	func viewWasCreated() {
+		return
+	}
+}
+
